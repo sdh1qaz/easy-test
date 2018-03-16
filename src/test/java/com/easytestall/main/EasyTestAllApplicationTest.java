c974cf91dacfa1a12a78d26de45f94af442911fc
苏登辉_科大讯飞 <dhsu2@iflytek.com>
2018-03-16 11:56:34 +0800

    更改src/test包名

--- src/test/java/com/easytestall/main/EasyTestAllApplicationTest.java
+++ src/test/java/com/easytestall/main/EasyTestAllApplicationTest.java
@@ -1,4 +1,4 @@
-package com.easytestall.demo;
+package com.easytestall.main;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class EasyTestAllApplicationTest {
 
 	@Test
 	public void contextLoads() {

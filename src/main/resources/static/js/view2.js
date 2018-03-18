c6b000641f961d2eb79dfce42d73b489f8b2a0b1
苏登辉_科大讯飞 <dhsu2@iflytek.com>
2018-03-19 00:41:55 +0800

    1.将view2.html页面中的js抽取出来放到单独的view2.js文件中。2.给ztree的父容器添加高度宽度样式，档展示内容较多时出现滚动条。

---
+++ src/main/resources/static/js/view2.js
@@ -0,0 +1,164 @@
+/**
+ * 在view2.html中应用的js
+ */
+
+var selectNode = 0;
+    var selectNodeAttr="";
+    function createTree(url, treeId) {
+        var zTree; //用于保存创建的树节点
+        var setting = { //设置
+            check: {
+                enable: true
+            },
+            view: {
+                showLine: true, //显示辅助线
+                dblClickExpand: true,
+            },
+            data: {
+                simpleData: {
+                    enable: true,
+                    idKey: "id",
+                    pIdKey: "pid",
+                    rootPId: 0
+                }
+            },
+            callback:{
+            	onClick:zTreeOnClick
+            }
+            
+        };
+         $.ajax({ //请求数据,创建树
+            type: 'GET',
+            url: "init/getNodes",
+            dataType: "json", //返回的结果为json  
+            success: function(data) {
+            	if(data==""||data==null)
+            		alert("接口数据初始化失败！！！\n\n请把 initail_tps.xls 放在 D 盘根目录下，\n\n表单元格不允许空或纯数字！！！");
+                zTree = $.fn.zTree.init($(treeId), setting, data); //创建树
+            },
+            error: function(data) {
+            	alert("接口数据初始化失败！！！\n请把 initail_tps.xls 放在 D 盘根目录下，\n表单元格不允许空或纯数字！！！");
+            }
+        }); 
+       
+    }
+      /*点击Ztree节点事件*/
+     function zTreeOnClick(event, treeId, treeNode) {
+          //alert(treeNode.attr);
+          //读取该接口的请求参数
+          var interFaceName = treeNode.attr;
+          selectNode = treeNode.rowNum;
+          selectNodeAttr = treeNode.attr;
+          if(interFaceName!=null && interFaceName!=""){
+        	  var url = "requestDto/getBody";
+              $.post(url,{"interfaceName":interFaceName},function(params){
+             	 $("#requestParam").val(params); 
+              })
+          }
+          else
+        	  $("#requestParam").val("");
+     }
+     
+     
+     
+     /*点击"复制返回报文"按钮，复制文本域内容到剪切板*/
+    function jsCopyResPon(){   
+        var e=document.getElementById("requestParam");//对象是contents   
+        e.select(); //选择对象   
+        tag=document.execCommand("Copy"); //执行浏览器复制命令  
+        if(!tag){  
+          alert('复制失败');  
+        }  
+    } 
+    
+    /*清空文本域，进度条归0*/
+    function clearResPon(){
+		  $("#returnText").val("");
+		  returnZero();
+		 //alert("你好");
+	 }
+    
+    /*采用递归的方法进行循环ajax调用*/
+    function Loop_ajax(index, array) {
+        if (index < array.length) {           
+            var param = array[index];
+            var cont = $("#returnText").val();
+            $.post("test/testBatchApi", {"param": param }, function (data) {
+           	 $("#returnText").val(cont + "\n****************************************************************\n"  + data);
+                console.log(array[index] + "," + data);
+                run(100/array.length,false); 
+                if (index < array.length) {
+                    Loop_ajax(index + 1, array);                    
+                }
+            });
+        }
+        else
+       	 run(100/array.length,true); 
+    }
+    
+    /*进度条归0*/
+    function returnZero(){
+    	var bar = document.getElementById("bar"); 
+    	bar.style.width = 0;
+    }
+    
+    /*文档加载完后激活后要做的*/
+    $(document).ready(function() {
+        createTree("jsonData.json", "#treeDemo"); //创建
+        $("#testing").hide();
+        $("#startTest").click(function() {
+        	returnZero();
+        	
+            var treeObj = $.fn.zTree.getZTreeObj("treeDemo");
+            var nodes = treeObj.getCheckedNodes(true);
+            if(0 === nodes.length) {
+                alert("请选择一个或多个接口!");
+                return;
+            }
+            
+            var dataNodes = "";
+            for(var i = 0; i < nodes.length; i++) {
+                if(nodes[i].attr!=""){
+                	if(i !=nodes.length-1)
+                		dataNodes += nodes[i].attr + ",";
+                	else
+                		dataNodes += nodes[i].attr;
+                }
+            }
+           //// alert(dataNodes); //获取选中节点的值
+            var array = dataNodes.split(",");
+            $("#testing").show();
+            Loop_ajax(0, array);
+            
+        });
+    });
+    
+    /*把excel中心加的接口信息更新到ztree*/
+    function updateRequParam(){
+  	  $.get("ztree/updateNodes",function(status){
+  		  //刷新当前页面
+  		  window.location.reload();
+  		  console.log(status);
+  	  })
+    }
+    
+    /*请求参数文本域点击保存*/
+    function saveRequParam(){
+  	  /* $("#requestParam").attr("readonly","readonly"); */
+  	  if(selectNode == 0 || selectNodeAttr=="")
+  		  return;
+  	  var params = $("#requestParam").val();
+  	  $.post("requestDto/updateBody",{"params":params,"rowNum":selectNode,"attrKey":selectNodeAttr},function(status){
+  	      alert(status);
+  	  })
+    }
+    
+	 function run(percent,isOver){ 
+		var bar = document.getElementById("bar"); 
+		if(isOver==false)
+		    bar.style.width=parseInt(bar.style.width) + percent + "%"; 
+		else{
+			bar.style.width="100%"; 
+		    $("#testing").hide();
+		}
+	  }  
